001f10f09041a6fcbfabd498565257f549d81b9c
Christophe Hurpeau <christophe@hurpeau.com>
2016-04-27 00:35:53 +0200

    use formatRecordToString

--- packages/nightingale-browser-console-formatter/browser/lib/index.js
+++ packages/nightingale-browser-console-formatter/browser/lib/index.js
@@ -9,105 +9,27 @@ var _nightingaleFormatter = require('nightingale-formatter');
 
 /**
  * @function
- * @param arr
+ * @param record
 */
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
-
-/**
- * @function
- * @param styles
- * @param string
-*/
-
-function style(styles, string) {
-    return styles.map(function (styleName) {
-        return _nightingaleFormatter.styleToHtmlStyle[styleName];
-    }).join('; ');
-}
-
-/**
- * @function
- * @param object
- * @param metadataStyles
-*/function displayObject(object, metadataStyles) {
-    var keys = Object.keys(object);
-
-    if (keys.length === 0) {
-        return;
-    }
-
-    args.push(style(['reset', 'gray']));
-    return '%c{ ' + keys.map(function (key) {
-        var _ref;
-
-        var styles = metadataStyles && metadataStyles[key] || [];
-        args.push(style(['reset', 'gray', 'bold']));
-        args.push(style((_ref = ['reset']).concat.apply(_ref, _toConsumableArray(styles))));
-        args.push(style(['reset', 'gray']));
-
-        return '%c' + key + ': %c' + JSON.stringify(object[key]) + '%c';
-    }).join(', ') + ' }';
-}
 
 /**
  * @param {Object} record
- * @returns {string}
+ * @returns {Array}
  */
-/**
- * @function
- * @param record
-*/function format(record) {
-    var string = '';
+function format(record) {
     var args = [];
-
-    if (record.key) {
-        string += '%c' + record.key;
-        args.push(style(['gray']));
-    }
-
-    if (record.datetime) {
-        if (string.length !== 0) {
-            string += ' ';
+    var string = (0, _nightingaleFormatter.formatRecordToString)(record, function (styles, string) {
+        if (!styles || !styles.length || !string) {
+            return string;
         }
 
-        args.push(style(['gray', 'bold']));
-        string += '%c' + record.datetime.toTimeString().split(' ')[0];
-    }
+        args.push(['reset'].concat(styles).map(function (styleName) {
+            return _nightingaleFormatter.styleToHtmlStyle[styleName];
+        }).join('; '));
+        args.push(_nightingaleFormatter.styleToHtmlStyle.reset);
+        return '%c' + string + '%c';
+    });
 
-    var message = record.symbol || _nightingaleFormatter.levelToSymbol[record.level];
-    var styles = record.styles || _nightingaleFormatter.levelToStyles[record.level];
-
-    if (record.message) {
-        if (message && message.length !== 0) {
-            message += ' ' + record.message;
-        } else {
-            message = record.message;
-        }
-    }
-
-    if (styles) {
-        message = '%c' + message;
-        args.push(style(styles));
-    }
-
-    if (string.length !== 0) {
-        string += ' ';
-    }
-
-    string += message;
-
-    if (record.metadata) {
-        var stringObject = displayObject(record.metadata, record.metadataStyles, args);
-
-        if (stringObject) {
-            if (string.length !== 0) {
-                string += ' ';
-            }
-
-            string += stringObject;
-        }
-    }
-
-    return [string].concat(args);
+    return [string, args];
 }
 //# sourceMappingURL=index.js.map
--- packages/nightingale-browser-console-formatter/lib/index.js
+++ packages/nightingale-browser-console-formatter/lib/index.js
@@ -9,95 +9,25 @@ var _nightingaleFormatter = require('nightingale-formatter');
 
 /**
  * @function
- * @param styles
- * @param string
+ * @param record
 */
-
-function style(styles, string) {
-    return styles.map(styleName => _nightingaleFormatter.styleToHtmlStyle[styleName]).join('; ');
-}
-
-/**
- * @function
- * @param object
- * @param metadataStyles
-*/function displayObject(object, metadataStyles) {
-    const keys = Object.keys(object);
-
-    if (keys.length === 0) {
-        return;
-    }
-
-    args.push(style(['reset', 'gray']));
-    return `%c{ ${ keys.map(key => {
-        const styles = metadataStyles && metadataStyles[key] || [];
-        args.push(style(['reset', 'gray', 'bold']));
-        args.push(style(['reset'].concat(...styles)));
-        args.push(style(['reset', 'gray']));
-
-        return `%c${ key }: %c${ JSON.stringify(object[key]) }%c`;
-    }).join(', ') } }`;
-}
 
 /**
  * @param {Object} record
- * @returns {string}
+ * @returns {Array}
  */
-/**
- * @function
- * @param record
-*/function format(record) {
-    let string = '';
+function format(record) {
     const args = [];
-
-    if (record.key) {
-        string += `%c${ record.key }`;
-        args.push(style(['gray']));
-    }
-
-    if (record.datetime) {
-        if (string.length !== 0) {
-            string += ' ';
+    const string = (0, _nightingaleFormatter.formatRecordToString)(record, (styles, string) => {
+        if (!styles || !styles.length || !string) {
+            return string;
         }
 
-        args.push(style(['gray', 'bold']));
-        string += `%c${ record.datetime.toTimeString().split(' ')[0] }`;
-    }
+        args.push(['reset'].concat(styles).map(styleName => _nightingaleFormatter.styleToHtmlStyle[styleName]).join('; '));
+        args.push(_nightingaleFormatter.styleToHtmlStyle.reset);
+        return `%c${ string }%c`;
+    });
 
-    let message = record.symbol || _nightingaleFormatter.levelToSymbol[record.level];
-    let styles = record.styles || _nightingaleFormatter.levelToStyles[record.level];
-
-    if (record.message) {
-        if (message && message.length !== 0) {
-            message += ` ${ record.message }`;
-        } else {
-            message = record.message;
-        }
-    }
-
-    if (styles) {
-        message = `%c${ message }`;
-        args.push(style(styles));
-    }
-
-    if (string.length !== 0) {
-        string += ' ';
-    }
-
-    string += message;
-
-    if (record.metadata) {
-        const stringObject = displayObject(record.metadata, record.metadataStyles, args);
-
-        if (stringObject) {
-            if (string.length !== 0) {
-                string += ' ';
-            }
-
-            string += stringObject;
-        }
-    }
-
-    return [string, ...args];
+    return [string, args];
 }
 //# sourceMappingURL=index.js.map
