9536bceda6d73b6612fd46ac61262d58b3c3388c
Christophe Hurpeau <christophe@hurpeau.com>
2016-04-27 01:08:51 +0200

    first commit

---
+++ packages/nightingale-error-processor/browser/lib/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = errorProcessor;
+
+var _alouette = require('alouette');
+
+/**
+ * @function
+ * @param record
+ * @param context
+*/function errorProcessor(record, context) {
+    if (!record.metadata) {
+        return;
+    }
+
+    record.extra = record.extra || {};
+
+    var error = record.metadata.error || record.metadata.err;
+
+    if (!(error instanceof Error)) {
+        return;
+    }
+    delete record.metadata.error;
+    delete record.metadata.err;
+
+    try {
+        var parsedError = (0, _alouette.parse)(error);
+        record.metadata.error = parsedError;
+    } catch (err) {
+        console.log(err.stack || err.message || err);
+        record.metadata.error = error;
+    }
+} /* eslint no-console: "off" */
+//# sourceMappingURL=index.js.map
---
+++ packages/nightingale-error-processor/src/index.js
@@ -0,0 +1,26 @@
+/* eslint no-console: "off" */
+import { parse as parseError } from 'alouette';
+
+export default function errorProcessor(record, context) {
+    if (!record.metadata) {
+        return;
+    }
+
+    record.extra = record.extra || {};
+
+    const error = record.metadata.error || record.metadata.err;
+
+    if (!(error instanceof Error)) {
+        return;
+    }
+    delete record.metadata.error;
+    delete record.metadata.err;
+
+    try {
+        const parsedError = parseError(error);
+        record.metadata.error = parsedError;
+    } catch (err) {
+        console.log(err.stack || err.message || err);
+        record.metadata.error = error;
+    }
+}
