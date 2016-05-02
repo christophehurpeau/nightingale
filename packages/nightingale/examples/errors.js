e9698fac8f9e7aa7fe3953e120b0800244b463dd
Christophe Hurpeau <christophe@hurpeau.com>
2016-05-02 16:53:41 +0200

    nightingale-logger@1.1.0, nightingale-json-formatter@1.1.0

--- packages/nightingale/examples/errors.js
+++ packages/nightingale/examples/errors.js
@@ -12,12 +12,5 @@ addGlobalProcessor(errorProcessor);
 
 const logger = new Logger('app');
 
-logger.log('test', { test: 'test', obj: { val: 1000 } });
-logger.log('test', { val: true });
-
-const timeStarted = logger.time();
-setTimeout(logger.timeEnd(timeStarted), 2000);
-
-// logger.error(new Error('test'));
-// logger.error('test', { error: new Error('test') });
-logger.success('Listening', { port: 3000 }, { port: ['yellow'] });
+logger.error(new Error('test'));
+logger.error('test', { error: new Error('test') });
