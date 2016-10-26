6acf77bdc6c4678f8028bfd57034f162afbbdd40
Christophe Hurpeau <christophe@hurpeau.com>
2016-10-26 22:40:37 +0200

    chore: pob update

--- packages/nightingale/index.js
+++ packages/nightingale/index.js
@@ -1,5 +1,4 @@
 var production = process.env.NODE_ENV === 'production';
 if (process.version.startsWith && process.version.startsWith('v6.'))
-  module.exports = require('./lib-node6' + (production ? '' : '-dev') + '/');
-else
-  module.exports = require('./lib-older-node' + (production ? '' : '-dev') + '/');
+    return module.exports = require('./lib-node6' + (production ? '' : '-dev') + '/');
+return module.exports = require('./lib-older-node' + (production ? '' : '-dev') + '/');
