ea3eb11ccb1aea746c07344b84b7061db6ebe9aa
Christophe Hurpeau <christophe@hurpeau.com>
2017-03-06 23:40:27 +0100

    fix: node 4

---
+++ packages/nightingale-console-output/lib-older-node/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _nightingaleLevels = require('nightingale-levels');
+
+var _nightingaleLevels2 = _interopRequireDefault(_nightingaleLevels);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+exports.default = function write(string, _ref) {
+  var level = _ref.level;
+
+  var outKey = level >= _nightingaleLevels2.default.ERROR ? 'stderr' : 'stdout';
+  process[outKey].write(string + '\n');
+};
+//# sourceMappingURL=index.js.map
