9cc902bce40359d32040e2ba78e3380c97a8c610
Christophe Hurpeau <christophe@hurpeau.com>
2017-03-21 23:48:00 +0100

    fix: node 6.10

--- packages/nightingale-ansi-formatter/index.js
+++ packages/nightingale-ansi-formatter/index.js
@@ -1,10 +1,12 @@
 var production = process.env.NODE_ENV === 'production';
-var nodeVersion = parseFloat(process.versions.node);
+var nodeVersion = process.versions.node.split('.');
+var nodeVersionMajor = Number(nodeVersion[0]);
+var nodeVersionMinor = Number(nodeVersion[1]);
 /* istanbul ignore next */
-if (nodeVersion >= 7.6)
+if (nodeVersionMajor > 7 || (nodeVersionMajor === 7 && nodeVersionMinor >= 6))
   return module.exports = require('./lib-node7' + (production ? '' : '-dev') + '/index');
 /* istanbul ignore next */
-if (nodeVersion >= 6.5)
+if (nodeVersionMajor > 6 || (nodeVersionMajor === 6 && nodeVersionMinor >= 5))
   return module.exports = require('./lib-node6' + (production ? '' : '-dev') + '/index');
 /* istanbul ignore next */
 return module.exports = require('./lib-older-node' + (production ? '' : '-dev') + '/index');
