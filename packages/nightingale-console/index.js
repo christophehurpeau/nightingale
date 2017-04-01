6939f4adfc20b6a89c162ac04cdab1d6cecff6ae
Christophe Hurpeau <christophe@hurpeau.com>
2017-04-01 15:20:39 +0200

    chore: pob upgrade

--- packages/nightingale-console/index.js
+++ packages/nightingale-console/index.js
@@ -1,8 +1,12 @@
 var production = process.env.NODE_ENV === 'production';
+var nodeVersion = process.versions.node.split('.');
+var nodeVersionMajor = Number(nodeVersion[0]);
+var nodeVersionMinor = Number(nodeVersion[1]);
 /* istanbul ignore next */
-if (process.version.startsWith && process.version.startsWith('v7.'))
+if (nodeVersionMajor > 7 || (nodeVersionMajor === 7 && nodeVersionMinor >= 6))
   return module.exports = require('./lib-node7' + (production ? '' : '-dev') + '/index');
 /* istanbul ignore next */
-if (process.version.startsWith && process.version.startsWith('v6.'))
+if (nodeVersionMajor > 6 || (nodeVersionMajor === 6 && nodeVersionMinor >= 5))
   return module.exports = require('./lib-node6' + (production ? '' : '-dev') + '/index');
+/* istanbul ignore next */
 return module.exports = require('./lib-older-node' + (production ? '' : '-dev') + '/index');
