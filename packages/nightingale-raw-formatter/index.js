efee51e70bbdc4ac008474cf088c52df1c045dbe
Christophe Hurpeau <christophe@hurpeau.com>
2017-04-01 15:44:21 +0200

    feat: support node 4

--- packages/nightingale-raw-formatter/index.js
+++ packages/nightingale-raw-formatter/index.js
@@ -9,4 +9,4 @@ if (nodeVersionMajor > 7 || (nodeVersionMajor === 7 && nodeVersionMinor >= 6))
 if (nodeVersionMajor > 6 || (nodeVersionMajor === 6 && nodeVersionMinor >= 5))
   return module.exports = require('./lib-node6' + (production ? '' : '-dev') + '/index');
 /* istanbul ignore next */
-throw new Error('Node version not supported: ' + nodeVersion + ' (' + process.versions.node + ').');
+return module.exports = require('./lib-older-node' + (production ? '' : '-dev') + '/index');
