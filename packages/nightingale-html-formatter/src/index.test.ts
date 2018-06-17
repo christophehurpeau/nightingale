d7a0f9cc73b7dd3dea6dd360c9ffc9d65693591d
Christophe Hurpeau <christophe@hurpeau.com>
2018-06-17 19:49:06 +0200

    fix: eslint errors

--- packages/nightingale-html-formatter/src/index.test.ts
+++ packages/nightingale-html-formatter/src/index.test.ts
@@ -1,5 +1,5 @@
 import Level from 'nightingale-levels';
-import format, { style } from './';
+import format, { style } from '.';
 
 test('blue bold color', () => {
   expect(style(['blue', 'bold'], 'test')).toBe(
