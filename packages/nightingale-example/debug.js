bddfe0a65932dff4df396d6e204b1234e41ac3b4
Christophe Hurpeau <christophe@hurpeau.com>
2020-11-15 13:45:38 +0100

    chore: pob update

--- packages/nightingale-example/debug.js
+++ packages/nightingale-example/debug.js
@@ -3,20 +3,4 @@
 'use strict';
 
 const production = process.env.NODE_ENV === 'production';
-const nodeVersion = process.versions.node.split('.');
-const nodeVersionMajor = Number(nodeVersion[0]);
-const nodeVersionMinor = Number(nodeVersion[1]);
-/* istanbul ignore next */
-if (
-  nodeVersionMajor > 12 ||
-  (nodeVersionMajor === 12 && nodeVersionMinor >= 10)
-) {
-  module.exports = require(`./dist/debug-node12${
-    production ? '' : '-dev'
-  }.cjs.js`);
-  /* istanbul ignore next */
-} else {
-  throw new Error(
-    `Node version not supported: ${nodeVersion} (${process.versions.node}).`,
-  );
-}
+module.exports = require(`./dist/debug-node12${production ? '' : '-dev'}.cjs`);
