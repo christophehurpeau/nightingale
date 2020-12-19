d0ef658581c58ee5882b817c23d14e910c775a0c
Christophe Hurpeau <christophe@hurpeau.com>
2020-12-19 19:15:34 +0100

    style: prettier

--- packages/nightingale-console-example/rollup.config.js
+++ packages/nightingale-console-example/rollup.config.js
@@ -5,7 +5,5 @@ const run = require('@rollup/plugin-run');
 const watch = process.env.ROLLUP_WATCH === 'true';
 
 module.exports = require('pob-babel/lib/createRollupConfig')({
-  devPlugins: [
-    watch && run({ execArgv: ['--enable-source-maps'] })
-  ],
+  devPlugins: [watch && run({ execArgv: ['--enable-source-maps'] })],
 });
