6cc377b2f87edf345d96236ad71cd5ce7cd9e797
Christophe Hurpeau <christophe@hurpeau.com>
2020-12-19 18:56:40 +0100

    chore: update dev dependencies

--- packages/nightingale-console-example/rollup.config.js
+++ packages/nightingale-console-example/rollup.config.js
@@ -1,3 +1,11 @@
 'use strict';
 
-module.exports = require('pob-babel/lib/createRollupConfig')();
+const run = require('@rollup/plugin-run');
+
+const watch = process.env.ROLLUP_WATCH === 'true';
+
+module.exports = require('pob-babel/lib/createRollupConfig')({
+  devPlugins: [
+    watch && run({ execArgv: ['--enable-source-maps'] })
+  ],
+});
