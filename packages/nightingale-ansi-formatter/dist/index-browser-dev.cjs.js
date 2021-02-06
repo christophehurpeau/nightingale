3582d4035f04c0c42e79de4e249f2aa76512816c
Christophe Hurpeau <christophe@hurpeau.com>
2021-02-06 09:15:47 +0100

    feat(nightingale-ansi-formatter): build for browser usage

---
+++ packages/nightingale-ansi-formatter/dist/index-browser-dev.cjs.js
@@ -0,0 +1,68 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var ansi = require('ansi-styles');
+var nightingaleFormatter = require('nightingale-formatter');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e['default'] : e; }
+
+var ansi__default = /*#__PURE__*/_interopDefaultLegacy(ansi);
+
+var ansiStyles = {
+  black: ansi__default.black,
+  red: ansi__default.red,
+  green: ansi__default.green,
+  yellow: ansi__default.yellow,
+  blue: ansi__default.blue,
+  magenta: ansi__default.magenta,
+  cyan: ansi__default.cyan,
+  white: ansi__default.white,
+  gray: ansi__default.gray,
+  bgBlack: ansi__default.bgBlack,
+  bgRed: ansi__default.bgRed,
+  bgGreen: ansi__default.bgGreen,
+  bgYellow: ansi__default.bgYellow,
+  bgBlue: ansi__default.bgBlue,
+  bgMagenta: ansi__default.bgMagenta,
+  bgCyan: ansi__default.bgCyan,
+  bgWhite: ansi__default.bgWhite,
+  bold: ansi__default.bold,
+  underline: ansi__default.underline,
+  // http://www.calmar.ws/vim/256-xterm-24bit-rgb-color-chart.html
+  orange: {
+    open: ansi__default.color.ansi256.hex(nightingaleFormatter.styleToHexColor.orange),
+    close: ansi__default.color.close
+  },
+  'gray-light': {
+    open: ansi__default.color.ansi256.hex(nightingaleFormatter.styleToHexColor['gray-light']),
+    close: ansi__default.color.close
+  }
+};
+function style(styles, string) {
+  if (!styles || styles.length === 0 || !string) {
+    return string;
+  }
+
+  return styles.reduce(function (string, styleName) {
+    var style = ansiStyles[styleName];
+
+    if (!style) {
+      throw new Error(`Unknown style: ${styleName}`);
+    }
+
+    return style.open + string + style.close;
+  }, string);
+}
+/**
+ * @param {Object} record
+ * @returns {string}
+ */
+
+function ansiFormat(record) {
+  return nightingaleFormatter.formatRecordToString(record, style);
+}
+
+exports.default = ansiFormat;
+exports.style = style;
+//# sourceMappingURL=index-browser-dev.cjs.js.map
---
+++ packages/nightingale-ansi-formatter/dist/index-browser-dev.es.js
@@ -0,0 +1,60 @@
+import ansi from 'ansi-styles';
+import { formatRecordToString, styleToHexColor } from 'nightingale-formatter';
+
+var ansiStyles = {
+  black: ansi.black,
+  red: ansi.red,
+  green: ansi.green,
+  yellow: ansi.yellow,
+  blue: ansi.blue,
+  magenta: ansi.magenta,
+  cyan: ansi.cyan,
+  white: ansi.white,
+  gray: ansi.gray,
+  bgBlack: ansi.bgBlack,
+  bgRed: ansi.bgRed,
+  bgGreen: ansi.bgGreen,
+  bgYellow: ansi.bgYellow,
+  bgBlue: ansi.bgBlue,
+  bgMagenta: ansi.bgMagenta,
+  bgCyan: ansi.bgCyan,
+  bgWhite: ansi.bgWhite,
+  bold: ansi.bold,
+  underline: ansi.underline,
+  // http://www.calmar.ws/vim/256-xterm-24bit-rgb-color-chart.html
+  orange: {
+    open: ansi.color.ansi256.hex(styleToHexColor.orange),
+    close: ansi.color.close
+  },
+  'gray-light': {
+    open: ansi.color.ansi256.hex(styleToHexColor['gray-light']),
+    close: ansi.color.close
+  }
+};
+function style(styles, string) {
+  if (!styles || styles.length === 0 || !string) {
+    return string;
+  }
+
+  return styles.reduce(function (string, styleName) {
+    var style = ansiStyles[styleName];
+
+    if (!style) {
+      throw new Error(`Unknown style: ${styleName}`);
+    }
+
+    return style.open + string + style.close;
+  }, string);
+}
+/**
+ * @param {Object} record
+ * @returns {string}
+ */
+
+function ansiFormat(record) {
+  return formatRecordToString(record, style);
+}
+
+export default ansiFormat;
+export { style };
+//# sourceMappingURL=index-browser-dev.es.js.map
