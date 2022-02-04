2e24ac5753ee386c5be9ca56796194ee598f10ff
Christophe Hurpeau <christophe@hurpeau.com>
2022-02-04 01:10:50 +0100

    fix: cjs build

---
+++ packages/nightingale-console-output/dist/index-node14.cjs
@@ -0,0 +1,16 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const nightingaleLevels = require('nightingale-levels');
+
+/* eslint-disable no-console */
+function consoleOutput(param, record) {
+  {
+    const outKey = record.level >= nightingaleLevels.Level.ERROR ? 'stderr' : 'stdout';
+    process[outKey].write(`${param}\n`);
+  }
+}
+
+exports["default"] = consoleOutput;
+//# sourceMappingURL=index-node14.cjs.map
---
+++ packages/nightingale-types/dist/index-node14.cjs
@@ -0,0 +1,10 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const nightingaleLevels = require('nightingale-levels');
+
+
+
+exports.Level = nightingaleLevels.Level;
+//# sourceMappingURL=index-node14.cjs.map
